92963ad706e22de6ce38c18ac2f26ac4cb50292a
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-11-15 15:07:21 -0300

    texto adicionado

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>APP Azure</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>Olá Professor</h1>
+    <p>Aqui está meu primeiro deploy de uma aplicação em PHP na Azure</p>
 </body>
 </html>

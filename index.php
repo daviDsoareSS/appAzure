a52824d50e62ed3df35afd77045e8e13363a6ef6
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-11-15 15:12:58 -0300

    texto adicionado

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 </head>
 <body>
     <h1>Olá Professor</h1>
-    <p>Aqui está meu primeiro deploy de uma aplicação em PHP na Azure</p>
+    <p>Aqui está meu primeiro deploy de uma aplicação em PHP na Azure.</p>
 </body>
 </html>
